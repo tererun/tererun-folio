93c1c2af4e4647460f01c6e185df1767cfa2354c
tererun <27857171+tererun@users.noreply.github.com>
2022-08-29 12:47:40 +0900

    :construction: WIP

--- features/TererunIcon/components/TererunIcon.tsx
+++ features/TererunIcon/components/TererunIcon.tsx
@@ -3,37 +3,39 @@ import React, {useEffect, useState} from "react";
 
 import styles from "../styles/TererunIconStyle.module.css";
 
-import Image from "next/image";
-
 import tererunNoEyesImage from "../../../public/images/tererun_no_eyes.png";
 import tererunAnimatedEyes from "../../../public/images/tererun_animated_eyes.gif";
 
 const TererunIcon: NextPage = () => {
-  const [left, setLeft] = useState(0);
-  const [top, setTop] = useState(0);
-
-  useEffect(() => {
-    document.addEventListener('mousemove', (e) => {
-      const calcX: number = (e.pageX / (window.innerWidth / 2) - 1) * 3;
-      const calcY: number = (e.pageY / (window.innerHeight / 2) - 1) * 3;
-      setLeft(calcX);
-      setTop(calcY);
+    const [location, setLocation] = useState({
+        left: 0,
+        top: 0
     });
-  });
 
-  return (
-      <div className="tererun-icon-wrapper">
-          <img className={styles.tererunNoEyesIcon} src={tererunNoEyesImage.src} />
-          <img
-              className={styles.tererunAnimatedEyes}
-              style={{
-                left: left,
-                top: top
-              }}
-              src={tererunAnimatedEyes.src}
-          />
+    useEffect(() => {
+        window.addEventListener('mousemove', (e) => {
+            const calcX: number = (e.pageX / (window.innerWidth / 2) - 1) * 3;
+            const calcY: number = (e.pageY / (window.innerHeight / 2) - 1) * 3;
+            setLocation({
+                left: calcX,
+                top: calcY
+            });
+        });
+    });
+
+    return (
+        <div className={styles.tererunIconWrapper}>
+            <img className={styles.tererunNoEyesIcon} src={tererunNoEyesImage.src} />
+            <img
+                className={styles.tererunAnimatedEyes}
+                style={{
+                    left: location.left,
+                    top: location.top
+                }}
+                src={tererunAnimatedEyes.src}
+            />
         </div>
-  )
+    )
 }
 
 export default TererunIcon;
